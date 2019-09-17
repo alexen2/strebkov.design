a7c3388cdaf19595d8ea9ee02fa5e6e2e5fd7bb5
Alex <vendmash23@gmail.com>
2019-09-17 17:10:29 +0300

    fix

---
+++ src/blocks/hamburger/hamburger.js
@@ -0,0 +1,23 @@
+var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+var hamburgers = document.querySelectorAll(".hamburger");
+if (hamburgers.length > 0) {
+  forEach(hamburgers, function(hamburger) {
+    hamburger.addEventListener("click", function() {
+      this.classList.toggle("is-active");
+      
+			var this_but = this;
+
+			function fixed_but() {
+			  this_but.style.position = this_but.style.position === 'fixed' ? '' : 'fixed';
+			  this_but.style.top = this_but.style.top === '26px' ? '' : '26px';
+			}
+
+			if(this.classList.contains("is-active")) {
+				setTimeout(fixed_but, 500);
+			} else {
+				this_but.style.position = this_but.style.position === 'fixed' ? '' : 'fixed';
+				this_but.style.top = this_but.style.top === '26px' ? '' : '26px';
+			}			
+    }, false);
+  });
+}
