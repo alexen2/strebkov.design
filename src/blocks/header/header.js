a60d8ea559409ee768486d26afa6180904d3dc12
Alex <vendmash23@gmail.com>
2019-09-18 18:56:28 +0300

    fix

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -2,11 +2,13 @@ jQuery(document).ready(function() {
 	jQuery("#hamburger").click(function () {
 		jQuery(this).toggleClass('close');
 		jQuery(".menu-mobile").toggleClass('menu-mobile--open');
+		jQuery('body').toggleClass('overflow-hidden');
 	});
 
 	jQuery(window).resize(function() {		
 		if (jQuery(window).width() > 767 ) {			
 			jQuery('.menu-mobile').removeClass('menu-mobile--open');
+			jQuery('body').removeClass('overflow-hidden');
 		}
 	});
 
@@ -15,9 +17,9 @@ jQuery(document).ready(function() {
 			 	st = $this.scrollTop();
 
 		if(st > 285){
-			$('.header').addClass('header--scrolled');
+			$('body').addClass('scrolled');
 		}else{
-			$('.header').removeClass('header--scrolled');
+			$('body').removeClass('scrolled');
 		}
 	});	
 });
