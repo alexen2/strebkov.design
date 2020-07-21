685c54e66637ee244d3e8727376de1610b363c9f
Andreikass <starforg-revolution@yandex.ru>
2020-07-21 18:45:47 +0500

    Add fix header and ho,e-top

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,25 +1,43 @@
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
 	jQuery("#hamburger").click(function () {
-		jQuery(this).toggleClass('close');
-		jQuery(".menu-mobile").toggleClass('menu-mobile--open');
-		jQuery('body').toggleClass('overflow-hidden');
+		jQuery(this).toggleClass("close");
+		jQuery(".menu-mobile").toggleClass("menu-mobile--open");
+		jQuery("body").toggleClass("overflow-hidden");
 	});
 
-	jQuery(window).resize(function() {		
-		if (jQuery(window).width() > 767 ) {			
-			jQuery('.menu-mobile').removeClass('menu-mobile--open');
-			jQuery('body').removeClass('overflow-hidden');
+	jQuery(window).resize(function () {
+		if (jQuery(window).width() > 767) {
+			jQuery(".menu-mobile").removeClass("menu-mobile--open");
+			jQuery("body").removeClass("overflow-hidden");
 		}
 	});
 
-	$(window).scroll(function(){
+	$(window).scroll(function () {
 		var $this = $(this),
-			 	st = $this.scrollTop();
+			st = $this.scrollTop();
 
-		if(st > 285){
-			$('.wrapper').addClass('scrolled');
-		}else{
-			$('.wrapper').removeClass('scrolled');
+		if (st > 285) {
+			$(".wrapper").addClass("scrolled");
+		} else {
+			$(".wrapper").removeClass("scrolled");
 		}
-	});	
+	});
+
+	$(".menu__item-dropdown").click(function () {
+		if (jQuery(window).width() < 1200) {
+			$(this).toggleClass("menu__item-dropdown-active");
+		}
+	});
+
+	jQuery(function ($) {
+		$(document).mouseup(function (e) {
+			let div = $(".menu__item-dropdown");
+			let user_bag = $(".menu__item-dropdown");
+			if (!div.is(e.target) && div.has(e.target).length === 0) {
+				if (!user_bag.is(e.target) && user_bag.has(e.target).length === 0) {
+					div.removeClass("menu__item-dropdown-active");
+				}
+			}
+		});
+	});
 });
