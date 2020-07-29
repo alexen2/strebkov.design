dac23ea91975224565c69059fcb0205e231580ac
Andreikass <starforg-revolution@yandex.ru>
2020-07-29 13:35:06 +0500

    add fix js header

--- src/blocks/menu/--in/--in.js
+++ src/blocks/menu/--in/--in.js
@@ -1,10 +1,24 @@
-if (jQuery(window).width() > 1199) {
-	$(".menu__item-dropdown").hover(
-		function () {
-			$(this).find(".menu__wrapper-dropdown").fadeIn(290);
-		},
-		function () {
-			$(this).find(".menu__wrapper-dropdown").fadeOut(290);
+jQuery(document).ready(function () {
+	let windowWidth = $(window).width();
+
+	menuDesctopHover(windowWidth);
+
+	jQuery(window).resize(function () {
+		windowWidth = $(window).width();
+		menuDesctopHover(windowWidth);
+	});
+
+	function menuDesctopHover(width) {
+		if (width > 1199) {
+			$(".menu__item-dropdown").hover(
+				function () {
+					$(this).find(".menu__wrapper-dropdown").fadeIn(290);
+					console.log(width);
+				},
+				function () {
+					$(this).find(".menu__wrapper-dropdown").fadeOut(290);
+				}
+			);
 		}
-	);
-}
+	}
+});
