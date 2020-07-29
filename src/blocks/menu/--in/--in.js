781bc007d7678561aba1f98215a5ad603654086d
Alexander Ponosov <psany@yandex.ru>
2020-07-29 09:40:15 +0500

    fix menu

--- src/blocks/menu/--in/--in.js
+++ src/blocks/menu/--in/--in.js
@@ -1,10 +1,10 @@
 if (jQuery(window).width() > 767) {
   $('.menu__item-dropdown').hover(
     function() {
-      $(this).find('.menu__wrapper-dropdown').fadeIn(400);
+      $(this).find('.menu__wrapper-dropdown').fadeIn(300);
     },
     function() {
-      $(this).find('.menu__wrapper-dropdown').fadeOut(400);
+      $(this).find('.menu__wrapper-dropdown').fadeOut(300);
     }
   );
 }
