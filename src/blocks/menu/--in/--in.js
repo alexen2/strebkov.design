b2a6be315c95a51bb7d3fbeabfa42684e434ed61
Alexander Ponosov <psany@yandex.ru>
2020-07-29 14:40:21 +0500

    fix menu

--- src/blocks/menu/--in/--in.js
+++ src/blocks/menu/--in/--in.js
@@ -1,24 +1,21 @@
 jQuery(document).ready(function () {
 	let windowWidth = $(window).width();
 
-	menuDesctopHover(windowWidth);
-
 	jQuery(window).resize(function () {
 		windowWidth = $(window).width();
-		menuDesctopHover(windowWidth);
+		$('.menu__item-dropdown').find(".menu__wrapper-dropdown").removeAttr('style');
 	});
 
-	function menuDesctopHover(width) {
-		if (width > 1199) {
-			$(".menu__item-dropdown").hover(
-				function () {
-					$(this).find(".menu__wrapper-dropdown").fadeIn(290);
-					console.log(width);
-				},
-				function () {
-					$(this).find(".menu__wrapper-dropdown").fadeOut(290);
-				}
-			);
+	$(".menu__item-dropdown").hover(
+		function () {
+			if(windowWidth > 1199) {
+				$(this).find(".menu__wrapper-dropdown").fadeIn(290);				
+			}	
+		},
+		function () {
+			if(windowWidth > 1199) {
+				$(this).find(".menu__wrapper-dropdown").fadeOut(290);
+			}
 		}
-	}
+	);
 });
