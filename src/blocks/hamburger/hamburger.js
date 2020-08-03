551c2a4881ccba465f81e809d61dccafa6f3a3c0
Andreikass <starforg-revolution@yandex.ru>
2020-08-03 12:12:30 +0500

    Add fix header and menu-mobile

--- src/blocks/hamburger/hamburger.js
+++ src/blocks/hamburger/hamburger.js
@@ -1,23 +1,30 @@
-var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
-var hamburgers = document.querySelectorAll(".hamburger");
-if (hamburgers.length > 0) {
-  forEach(hamburgers, function(hamburger) {
-    hamburger.addEventListener("click", function() {
-      this.classList.toggle("is-active");
-      
-			var this_but = this;
+// var forEach = function (t, o, r) {
+// 	if ("[object Object]" === Object.prototype.toString.call(t)) for (var c in t) Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t);
+// 	else for (var e = 0, l = t.length; l > e; e++) o.call(r, t[e], e, t);
+// };
+// var hamburgers = document.querySelectorAll("#hamburger");
+// if (hamburgers.length > 0) {
+// 	forEach(hamburgers, function (hamburger) {
+// 		hamburger.addEventListener(
+// 			"click",
+// 			function () {
+// 				this.classList.toggle("is-active");
 
-			function fixed_but() {
-			  this_but.style.position = this_but.style.position === 'fixed' ? '' : 'fixed';
-			  this_but.style.top = this_but.style.top === '25px' ? '' : '25px';
-			}
+// 				// var this_but = this;
 
-			if(this.classList.contains("is-active")) {
-				setTimeout(fixed_but, 500);
-			} else {
-				this_but.style.position = this_but.style.position === 'fixed' ? '' : 'fixed';
-				this_but.style.top = this_but.style.top === '25px' ? '' : '25px';
-			}			
-    }, false);
-  });
-}
+// 				// function fixed_but() {
+// 				//   this_but.style.position = this_but.style.position === 'fixed' ? '' : 'fixed';
+// 				//   this_but.style.top = this_but.style.top === '25px' ? '' : '25px';
+// 				// }
+
+// 				// if(this.classList.contains("is-active")) {
+// 				// 	setTimeout(fixed_but, 500);
+// 				// } else {
+// 				// 	this_but.style.position = this_but.style.position === 'fixed' ? '' : 'fixed';
+// 				// 	this_but.style.top = this_but.style.top === '25px' ? '' : '25px';
+// 				// }
+// 			},
+// 			false
+// 		);
+// 	});
+// }
--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,16 +1,24 @@
 jQuery(document).ready(function () {
 	jQuery("#hamburger").click(function () {
-		jQuery(this).toggleClass("close");
-		jQuery(".menu-mobile").toggleClass("menu-mobile--open");
-		jQuery(".overlay").toggleClass("overlay--open");
-		jQuery("body").toggleClass("overflow-hidden");
+		jQuery(".hamburger-mobile").addClass("close is-active");
+		jQuery(".hamburger").addClass("is-active");
+		jQuery(".menu-mobile").addClass("menu-mobile--open");
+		jQuery(".overlay").addClass("overlay--open");
+		jQuery("body").addClass("overflow-hidden");
 	});
-
+	jQuery(".hamburger-mobile").click(function () {
+		jQuery(this).removeClass("close is-active");
+		jQuery(".hamburger").removeClass("is-active");
+		jQuery(".menu-mobile").removeClass("menu-mobile--open");
+		jQuery(".overlay").removeClass("overlay--open");
+		jQuery("body").removeClass("overflow-hidden");
+	});
 	jQuery(window).resize(function () {
 		if (jQuery(window).width() > 767) {
 			jQuery(".menu-mobile").removeClass("menu-mobile--open");
 			jQuery("body").removeClass("overflow-hidden");
 			jQuery(".overlay").removeClass("overlay--open");
+			jQuery(".hamburger").removeClass("is-active close");
 		}
 	});
 
